78b537f248a8da12d29836b1add6380525159fe2
alexandermasud <alex@Alex-iMac.local>
2017-09-19 23:46:47 +0200

    Påbörjar REST DELETE
    Får det inte att fungera, kan inte returnera query.

--- routes/deleteTransaction.js
+++ routes/deleteTransaction.js
@@ -4,36 +4,23 @@ var pg = require('pg');
 var conString = "postgres://mtmjbqma:FV-Pmc7MOX4BPDO_8CUE7n9lBFaFMp-d@horton.elephantsql.com:5432/mtmjbqma";
 
 
-router.delete('/deleteTransaction/:id', function(req, res) {
+router.post('/deleteTransaction:id', function(req, res) {
 	pg.connect(conString, function(err, client, done) {
 		if (err) {
 			return console.error('error while fetching client from pool', err);
 		}
-        
-        
-       
-		client.query("SELECT * FROM resa", function(err, result, fields) {
+		var transaktionsid = (req.body.transaktionsid)
+		var avgangsid = (req.body.avgangsid)
+		var platser = (req.body.platser)
+		var queryLine = "BEGIN; DELETE FROM kop WHERE transaktionsid ='" + (req.body.transaktionsid) + "'; UPDATE resa SET platser = platser + '" + (req.body.platser) + "' WHERE avgangsid = '" + (req.body.avgangsid) + "' ; COMMIT;"
+		client.query(queryLine, function(err, result) {
 			if (err) {
-				return console.error('error while fetching client from pool', err);
+				return console.error('error running query', err);
 			}
-            
-            else{
-                
-                
-                
-                console.log('Resultat =' + result)
-                
-            }
-                
-                
-          
-                
-            
-                
-              
-			
+			req.flash('test_msg', '<p>Transaktion ' + (req.body.transaktionsid) + ' togs bort!</p>');
+			res.redirect('/transactions');
+			done();
 		});
 	});
 });
 module.exports = router;
-
